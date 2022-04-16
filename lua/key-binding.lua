93088f45454c7fcdd657198f718ef7978ba7e912
yangxing <yangxing844@gmail.com>
2022-04-16 16:16:19 +0800

    move the format keybinding for null-ls to use it

--- lua/key-binding.lua
+++ lua/key-binding.lua
@@ -179,3 +179,5 @@ vim.api.nvim_set_keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<
 	noremap = true,
 	silent = true,
 })
+
+vim.api.nvim_set_keymap("n", "ff", "<cmd>lua vim.lsp.buf.formatting_sync()<CR>", { noremap = true, silent = true })
